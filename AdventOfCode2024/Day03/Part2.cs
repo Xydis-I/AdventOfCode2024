656ec4c347a94e945cdf5134484f573735f01af3
C. C. Barrett <ccb9952@hotmail.com>
2024-12-09 20:15:05 -0330

    Day 3 Part 2 done.

--- AdventOfCode2024/Day03/Part2.cs
+++ AdventOfCode2024/Day03/Part2.cs
@@ -1,8 +1,10 @@
-﻿namespace AdventOfCode2024.Day03
+﻿using System.Text.RegularExpressions;
+
+namespace AdventOfCode2024.Day03
 {
     internal static class Part2
     {
-        private const string FileLocation = @"..\..\..\Inputs\Day02_Input.txt";
+        private const string FileLocation = @"..\..\..\Inputs\Day03_Input.txt";
 
         public static void Solution()
         {
@@ -12,12 +14,37 @@
                 var lines = input.ReadToEnd().Split("\r\n");
 
                 var total = 0;
+
+                var instructions = new List<Match>();
+
                 foreach (var line in lines)
                 {
-                    var numbers = line.Split(" ").Select(int.Parse).ToList();
+                    instructions.AddRange(Regex.Matches(line, "(mul\\()([0-9]{1,3})(,)([0-9]{1,3})(\\))|(don't\\(\\))|(do\\(\\))"));
+                }
+
+                var isDisabled = false;
+
+                foreach (var instruction in instructions)
+                {
+                    var isMul = true;
+
+                    if (instruction.Value == "do()")
+                    {
+                        isDisabled = false;
+                        isMul = false;
+                    }
+
+                    if (instruction.Value == "don't()")
+                    {
+                        isDisabled = true;
+                        isMul = false;
+                    }
 
 
-
+                    if (!isDisabled && isMul)
+                    {
+                        total += int.Parse(instruction.Groups[2].Value) * int.Parse(instruction.Groups[4].Value);
+                    }
                 }
 
                 Console.WriteLine("Day02_Part1 Answer: " + total);
